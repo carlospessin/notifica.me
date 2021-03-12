d48a08f16c5bf10e024796f4c56f8d03d4df802c
pessin <carlospessin@gmail.com>
2021-03-12 10:30:08 -0300

    emitindo notificação ao término do timer

--- src/app.js
+++ src/app.js
@@ -1,16 +1,25 @@
 import { Notifier } from './Notifier.js';
 import { Timer } from './Timer.js';
+import { Emitter } from './Emitter.js';
+
+const notify = Notifier.notify({
+  title: "11:00",
+  body: "Conteúdo da notificação"
+})
+
 
 const App = {
   async start() {
     try {
+      await Notifier.init()
+
+      Emitter.on('countdown-start', notify)
+      Emitter.on('countdown-end', Timer.init)
+
       const time = 25 * 60
       Timer.init(time);
-      // await Notifier.init()
-      // Notifier.notify({
-      //   title: "11:00",
-      //   body: "Conteúdo da notificação"
-      // })
+
+
     } catch (err) {
       console.log(err.message)
     }
